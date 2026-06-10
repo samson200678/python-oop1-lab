6e3907ded8754a11ed0cc2017d643ac1fbae2ba1
samson kangara <samkangara06@gmail.com>
2026-06-10 08:39:24 +0300

    feat:add .

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+def _init_(self, size, price):
+        self.size = size
+        self.price = price
 
-class Coffee:
-    pass
+@property
+def size(self):
+    return self._size
+
+@size.setter
+def size(self, value):
+    if value in ["Small", "Medium", "Large"]:
+        self._size = value
+    else:
+            print("size must be Small, Medium, or Large")
+
+def tip(self):
+    print("This coffee is great, here's a tip!")
+    self.price += 1
